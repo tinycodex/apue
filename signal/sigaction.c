90202915e718341b4ade3dd05e4a2037e4ada581
soul11201 <soul11201@gmail.com>
2024-06-24 20:15:01 +0800

    sigaction

---
+++ signal/sigaction.c
@@ -0,0 +1,54 @@
+//
+// Created by X on 2024/6/12.
+//
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+void sig_handler(int signo)
+{
+    printf("signo:%d begin\n", signo);
+    write(1, "!", 1);
+    sleep(5);
+    printf("signo:%d end\n", signo);
+}
+
+void sigaction_handler(int signo, siginfo_t *infop, void *unused)
+{
+    printf("signo:%d si_code:%d\n", infop->si_signo, infop->si_code);
+}
+
+int main(int argc, char *argv[])
+{
+    printf("[%d]hello, world\n", getpid());
+    printf("%d\n",SI_USER);
+
+    struct sigaction sa;
+
+    sa.sa_sigaction = sigaction_handler;
+    sa.sa_flags = SA_SIGINFO;
+    // sigemptyset(&sa.sa_mask);
+    // sigaddset(&sa.sa_mask, SIGINT);
+    // sigaddset(&sa.sa_mask, SIGTERM);
+    // sigaddset(&sa.sa_mask, SIGQUIT);
+
+    sigaction(SIGALRM, &sa, NULL);
+    sigaction(SIGINT, &sa, NULL);
+    // sigaction(SIGTERM, &sa, NULL);
+    // sigaction(SIGQUIT, &sa, NULL);
+
+    alarm(1);
+
+    // signal(SIGINT, sig_handler);
+    // signal(SIGQUIT,sig_handler);
+    // signal(SIGTERM,sig_handler);
+
+    for (int i = 0; i < 1000; ++i)
+    {
+        write(1, "*", 1);
+        sleep(1);
+    }
+
+    return 0;
+}
--- signal/signal.c
+++ signal/signal.c
@@ -4,22 +4,28 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
-
+#include <stdio.h>
 
 void sig_handler(int signo){
+    printf("signo:%d begin\n",signo);
     write(1,"!",1);
-    sleep(2);
+    sleep(5);
+    printf("signo:%d end\n",signo);
 }
 
 
 int main(int argc,char*argv[]){
-    printf("hello, world\n");
+    printf("[%d]hello, world\n",getpid());
+
 
 //    signal(SIGINT, SIG_IGN);
 
     signal(SIGINT, sig_handler);
+    signal(SIGQUIT,sig_handler);
+    signal(SIGTERM,sig_handler);
+    
 
-    for(int i = 0;i < 10;++i){
+    for(int i = 0;i < 1000;++i){
         write(1,"*",1);
         sleep(1);
     }
